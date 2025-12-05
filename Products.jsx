0fed48c89083b11be4ceb25100ac83753b5cf0f1
Berkant Gunes <gunesberkant96@gmail.com>
2025-12-05 17:15:48 +0300

    Add files via upload

---
+++ Products.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+
+
+const Products = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const res = await fetch("http://localhost:5000/api/products/get-all");
+                const data = await res.json();
+                setProducts(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        getProducts();
+    }, []);
+
+    return (
+        <div className="products-wrapper grid grid-cols-card gap-4">
+            {products.map((item) => (
+                <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+                    <div className="product-img">
+                        <img src={item.img} alt="" className="h-28 object-cover w-full border-b" />
+                    </div>
+                    <div className="product-info flex flex-col p-3">
+                        <span className="font-bold">{item.title}</span>
+                        <span>{item.price}$</span>
+                    </div>
+                </div>
+            ))}
+
+        </div>
+    )
+}
+
+export default Products
