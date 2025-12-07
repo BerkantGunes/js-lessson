a6fa625b2f95204d8dab495276f86517f17ab734
Berkant Gunes <gunesberkant96@gmail.com>
2025-12-07 12:01:47 +0300

    Add files via upload

--- Edit.jsx
+++ Edit.jsx
@@ -1,83 +1,173 @@
-import { Button, Form, Input, message, Modal, Table } from 'antd'
-import React, { useState } from 'react'
+import { Button, Form, Input, message, Select, Modal, Table } from 'antd'
+import React, { useState, useEffect } from 'react'
 
-const Edit = ({ isEditModalOpen, setIsEditModalOpen, categories, setCategories }) => {
-  const [editingRow, setEditingRow] = useState({});
+const Edit = () => {
+    const [products, setProducts] = useState([])
+    const [categories, setCategories] = useState([])
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+    const [editingItem, setEditingItem] = useState({});
+    const [form] = Form.useForm();
 
-  const onFinish = (values) => {
-    try {
-      fetch("http://localhost:5000/api/categories/update-category"), {
-        method: "PUT",
-        body: JSON.stringify(...values, { _id: editingRow._id }),
-        headers: { "Content-type": "application/json; charset=UTF-8" }
-      }
-      message.success("Category updated successfully!")
-      setCategories(categories.map((item) => {
-        if (item._id === editingRow._id) {
-          return { ...item, title: values.title }
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const res = await fetch("http://localhost:5000/api/products/get-all");
+                const data = await res.json();
+                setProducts(data);
+            } catch (error) {
+                console.log(error);
+            }
         }
-        return item;
-      }));
-    }
-    catch (error) {
-      message.error("Something went wrong..")
-      console.log(error);
-    }
-  }
 
-  const deleteCategory = (id) => {
-    try {
-      fetch("http://localhost:5000/api/categories/delete-category", {
-        method: "DELETE",
-        body: JSON.stringify({ _id: id }),
-        headers: { "Content-type": "application/json; charset=UTF-8" }
-      })
-      message.success("Category deleted successfully!")
-      setCategories(categories.filter((item) => item._id !== id));
-    }
-    catch (error) {
-      console.log(error);
-      message.error("Something went wrong..")
-    }
-  }
+        getProducts();
+    }, []);
+    useEffect(() => {
+        const getCategories = async () => {
+            try {
+                const res = await fetch("http://localhost:5000/api/categories/get-all");
+                const data = await res.json();
+                data && setCategories(
+                    data.map((item) => {
+                        return { ...item, value: item.title };
+                    })
+                );
+                
+            } catch (error) {
+                console.log(error);
+            }
+        }
 
-  const columns = [{
-    title: "Category Title",
-    dataIndex: "title",
-    render: (_, record) => {
-      if (record._id === editingRow._id) {
-        return (
-          <Form.Item className='mb-0' name="title">
-            <Input defaultValue={record.title} />
-          </Form.Item>
-        );
-      } else {
-        return <p>{record.title}</p>
-      }
+        getCategories();
+    }, []);
+
+    const onFinish = (values) => {
+        try {
+            fetch("http://localhost:5000/api/products/update-product"), {
+                method: "PUT",
+                body: JSON.stringify({ ...values, productId: editingItem._id }),
+                headers: { "Content-type": "application/json; charset=UTF-8" }
+            }
+            message.success("Product updated successfully!")
+            setProducts(products.map((item) => {
+                if(item._id === editingItem._id) {
+                    return values;
+                }
+                return item;
+            }));
+        }
+        catch (error) {
+            message.error("Something went wrong..")
+            console.log(error);
+        }
     }
-  },
-  {
-    title: "Action",
-    dataIndex: "action",
-    render: (_, record) => {
-      return (
-        <div>
-          <Button type="link" onClick={() => setEditingRow(record)}>Edit</Button>
-          <Button type="text" htmlType='submit'>Save</Button>
-          <Button type="text" danger onClick={() => deleteCategory(record._id)}>Delete</Button>
-        </div>
-      );
+
+    const deleteProduct = (id) => {
+        try {
+            fetch("http://localhost:5000/api/products/delete-product", {
+                method: "DELETE",
+                body: JSON.stringify({ productId: id }),
+                headers: { "Content-type": "application/json; charset=UTF-8" }
+            })
+            message.success("Product deleted successfully!")
+            setProducts(products.filter((item) => item._id !== id));
+        }
+        catch (error) {
+            console.log(error);
+            message.error("Something went wrong..")
+        }
+    }
+
+    const columns = [{
+        title: "Product Title",
+        dataIndex: "title",
+        width: "8%",
+        render: (_, record) => {
+            return <p>{record.title}</p>
+        }
     },
-  },
-  ]
+    {
+        title: "Product Image",
+        dataIndex: "img",
+        width: "4%",
+        render: (_, record) => {
+            return <img src={record.img} alt="" className='w-full h-20 object-cover' />
+        }
+    },
+    {
+        title: "Price",
+        dataIndex: "price",
+        width: "8%"
+    },
+    {
+        title: "Category",
+        dataIndex: "category",
+        width: "8%"
+    },
+    {
+        title: "Action",
+        dataIndex: "action",
+        width: "8%",
+        render: (_, record) => {
+            return (
+                <div>
+                    <Button type="link" className='pl-0' onClick={() => { 
+                        setIsEditModalOpen(true)
+                        setEditingItem(record) }}>Edit</Button>
+                    <Button type="text" danger onClick={() => deleteCategory(record._id)}>Delete</Button>
+                </div>
+            );
+        },
+    },
+    ]
 
-  return (
-    <Modal open={isEditModalOpen} title="Category Options" footer={false} onCancel={() => setIsEditModalOpen(false)}>
-      <Form onFinish={onFinish}>
-        <Table bordered dataSource={categories} columns={columns} />
-      </Form>
-    </Modal>
-  )
+    return (
+        <>
+            <Table bordered dataSource={products} columns={columns} rowKey={"_id"}
+                scroll={{
+                    x: 1000,
+                    y: 600,
+                }} />
+
+            <Modal title="Add New Product" open={isEditModalOpen} onCancel={() => setIsEditModalOpen(false)} footer={false}>
+                <Form layout='vertical' onFinish={onFinish} form={form} initalValues={editingItem}>
+                    <Form.Item name="title" label="Product Name" rules={[{ required: true, message: "Product Name field can not be null!" }]}>
+                        <Input placeholder='Enter product name' />
+                    </Form.Item>
+
+                </Form>
+                <Form layout='vertical' onFinish={onFinish} form={form}>
+                    <Form.Item name="img" label="Product Image" rules={[{ required: true, message: "Product image field can not be null!" }]}>
+                        <Input placeholder='Enter product image' />
+                    </Form.Item>
+             </Form>
+                <Form layout='vertical' onFinish={onFinish} form={form}>
+                    <Form.Item name="price" label="Product Price" rules={[{ required: true, message: "Product price field can not be null!" }]}>
+                        <Input placeholder='Enter product price' />
+                    </Form.Item>
+
+                </Form>
+                <Form layout='vertical' onFinish={onFinish} form={form}>
+                    <Form.Item name="category" label="Product Category" rules={[{ required: true, message: "Product category field can not be null!" }]}>
+                        <Select showSearch placeholder="Search to Select" optionFilterProp='children' filterOption={
+                            (input, option) => (option?.title ?? "").includes(input)
+                        }
+                            filterSort={(optionA, optionB) => (optionA?.title ?? "")
+                                .toLowerCase()
+                                .localeCompare((optionB?.title ?? "").toLowerCase())
+                            }
+                            options={categories} />
+                    </Form.Item>
+
+                </Form>
+
+                <Form.Item className='flex justify-end mb-0'>
+                    <Button type='primary' htmlType='submit'>Update</Button>
+                </Form.Item>
+            </Modal>
+        </>
+
+
+    )
 }
 
 export default Edit
